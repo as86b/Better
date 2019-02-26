1c5b14d9641531ce1acfdababcbcd50988eaf769
Zach Dobbs <dobbszach@gmail.com>
2019-02-26 00:32:51 -0600

    Fix schema reference definitions

--- model/Tag.js
+++ model/Tag.js
@@ -8,7 +8,7 @@ const Post = require('./Post');
 
 const TagSchema = mongoose.Schema({
     tag_name: {type: String, unique: true},                        // primary key
-    posts: [{ _id: mongoose.Schema.Types.ObjectId, ref: 'Post' }]    
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]    
 });
 
 const Tag = module.exports = mongoose.model('Tag', TagSchema);

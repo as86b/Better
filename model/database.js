c3559e65852abe355d0e536d42ea321dcffb1db0
Main <Kraken>
2019-03-10 21:08:56 -0500

    Ctrl C database handling

--- model/database.js
+++ model/database.js
@@ -6,9 +6,21 @@ mongoose.connect(
     'mongodb://' + config.ipaddr + ':' + config.port,
     { user: config.user, pass: config.pass }
 );
+/*
+this connection should be available to all other
+modules that require('mongoose'), because *magic*
+*/
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    console.log('connected to database')
+    console.log('Connected to database');
+});
+
+process.on('SIGINT', function() {
+    console.log('Severing MongoDB connection...');
+  mongoose.connection.close(function () {
+    console.log('Disconnected from MongoDB');
+    process.exit(0);
+  });
 });

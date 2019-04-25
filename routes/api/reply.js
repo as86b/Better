cfb88959ca5bf7217f1565d19b98d980e8f2886f
Kristyn Long <kristynlong@Kristyns-MacBook-Pro.local>
2019-04-25 17:45:49 -0500

    fix mistake by Austing

--- routes/api/reply.js
+++ routes/api/reply.js
@@ -18,11 +18,7 @@ async function addReply(username, postID, body, anon, res) {
 		});
 		return;
 	}
-<<<<<<< HEAD
 	
-=======
-	console.log(doc);
->>>>>>> fc0466bb410c0a8eff48ff9fccd5d9dadf866b37
 	var reply = new Reply({
 		user_id: doc._id,
 		post_id: postID,
